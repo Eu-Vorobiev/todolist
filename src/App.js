70ed13632691486051b894bf1e2b3b1f64aed72a
Yevhenii Vorobiov <dijjekas06@gmail.com>
2023-03-05 16:16:38 +0200

    Date optimization

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@ import React, { useState } from 'react';
 import Header from './components/Header/Header';
 import Container from './components/Container/Container';
 import ToDoList from './components/ToDoList/ToDoList';
+import { getDate } from './components/Date/Date';
 
 function App() {
-  let initTasks = [
-    { id: 1, title: 'Task 1', duration: 1, date: '2020-12-12', completed: false},
-    { id: 2, title: 'Task 2', duration: 2, date: '2020-12-12', completed: false},
-  ]
-  
-  const [tasks, setTasks] = useState(initTasks);
+  const [tasks, setTasks] = useState([]);
+  const { ampm } = getDate();
   
   const addTask = ({ title, duration, completed = false }) => {
     let formattedDuration = duration / 60;
@@ -18,10 +15,6 @@ function App() {
     let currentDate = new Date();
     let hours = currentDate.getHours();
     let minutes = currentDate.getMinutes();
-    let ampm = hours >= 12 ? 'PM' : 'AM';
-    hours = hours % 12;
-    hours = hours ? hours : 12;
-    minutes = minutes < 10 ? '0' + minutes : minutes;
     formattedDate = `${hours}:${minutes} ${ampm}`;
     
     let newTask = { id: Math.random(), title: title, duration: formattedDuration, date: formattedDate, completed: completed }
@@ -40,13 +33,11 @@ function App() {
     setTasks(newTasks);
   }
 
-  let newSettedTask = tasks;
-
   return (
     <>
       <Header />
       <Container>
-        <ToDoList tasks={newSettedTask} addTask={addTask} removeTask={removeTask} completeTask={completeTask} />
+        <ToDoList tasks={tasks} addTask={addTask} removeTask={removeTask} completeTask={completeTask} />
       </Container>
     </>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,44 +1,9 @@
 import React from 'react';
 import './Header.css';
+import { getDate } from '../Date/Date';
 
 function Header() {
-  let date = new Date();
-  let day = date.getDay();
-  let dayNum = date.getDate();
-  let dayName = '';
-  switch (day) {
-    case 0:
-      dayName = 'sun';
-      break;
-    case 1:
-      dayName = 'mon';
-      break;
-    case 2:
-      dayName = 'tue';
-      break;
-    case 3:
-      dayName = 'wed';
-      break;
-    case 4:
-      dayName = 'thu';
-      break;
-    case 5:
-      dayName = 'fri';
-      break;
-    case 6:
-      dayName = 'sat';
-      break;
-    default:
-      dayName = 'sun';
-      break;
-  }
-
-  let hours = date.getHours();
-  let minutes = date.getMinutes();
-  let ampm = hours >= 12 ? 'PM' : 'AM';
-  hours = hours % 12;
-  hours = hours ? hours : 12;
-  minutes = minutes < 10 ? '0' + minutes : minutes;
+  const { dayName, dayNum, hours, minutes, ampm } = getDate();
 
   return (
     <header className="header">
