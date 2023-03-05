dc01a093d74bfd52f3367cb090fbea53b3bce43e
Yevhenii Vorobiov <dijjekas06@gmail.com>
2023-03-05 14:35:37 +0200

    Header's datetime

--- src/App.js
+++ src/App.js
@@ -5,15 +5,15 @@ import ToDoList from './components/ToDoList/ToDoList';
 
 function App() {
   let initTasks = [
-    { id: 1, title: 'Task 1', duration: 1, date: '2020-12-12' },
-    { id: 2, title: 'Task 2', duration: 2, date: '2020-12-12' },
+    { id: 1, title: 'Task 1', duration: 1, date: '2020-12-12', completed: false},
+    { id: 2, title: 'Task 2', duration: 2, date: '2020-12-12', completed: false},
   ]
   
   const [tasks, setTasks] = useState(initTasks);
   
-  const addTask = ({ title, duration, date = '' }) => {
+  const addTask = ({ title, duration, date = '', completed = false }) => {
     let formattedDuration = duration / 60;
-    let newTask = { id: Math.random(), title: title, duration: formattedDuration, date: date }
+    let newTask = { id: Math.random(), title: title, duration: formattedDuration, date: date, completed: completed }
     let newTasks = [newTask, ...tasks];
     setTasks(newTasks);
   }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,9 +2,50 @@ import React from 'react';
 import './Header.css';
 
 function Header() {
+  let date = new Date();
+  let day = date.getDay();
+  let dayNum = date.getDate();
+  let dayName = '';
+  switch (day) {
+    case 0:
+      dayName = 'sun';
+      break;
+    case 1:
+      dayName = 'mon';
+      break;
+    case 2:
+      dayName = 'tue';
+      break;
+    case 3:
+      dayName = 'wed';
+      break;
+    case 4:
+      dayName = 'thu';
+      break;
+    case 5:
+      dayName = 'fri';
+      break;
+    case 6:
+      dayName = 'sat';
+      break;
+    default:
+      dayName = 'sun';
+      break;
+  }
+
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let ampm = hours >= 12 ? 'PM' : 'AM';
+  hours = hours % 12;
+  hours = hours ? hours : 12;
+  minutes = minutes < 10 ? '0' + minutes : minutes;
+
   return (
     <header className="header">
-      <time><span>thu 9</span>6:23 AM</time>
+      <time>
+        <span className='day'>{dayName} {dayNum}</span>
+        <span>{hours}<span className='pulse'>:</span>{minutes} {ampm}</span>
+      </time>
     </header>
   );
 }
